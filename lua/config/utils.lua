465a73e8fbe5b3a47ae668d83a5b24c4823e09cf
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-14 20:35:08 +0330

    docs(utils): add type annotations

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,44 +1,65 @@
-function table.unique(tbl)
+---Uniquifies list.
+---@generic T
+---@param list T[] List to be uniquified.
+---@return T[]
+function table.unique(list)
   local ret, hash = {}, {}
-  for _, value in ipairs(tbl) do
-    if not hash[value] then ret[#ret + 1] = value end
+  for _, value in ipairs(list) do
+    if not hash[value] then table.insert(ret, value) end
     hash[value] = true
   end
 
   return ret
 end
 
+---Merges lists in each other.
+---@generic T
+---@param ... T[] Lists to be merged.
+---@return T[]
 function table.merge(...)
   local ret = {}
-  for _, tbl in ipairs({ ... }) do
-    for key, value in pairs(tbl) do
-      ret[type(key) == "number" and #ret + 1 or key] = value
+  for _, list in ipairs({ ... }) do
+    for _, value in ipairs(list) do
+      table.insert(ret, value)
     end
   end
 
   return ret
 end
 
-function table.filter(tbl, callback)
+---Determines if an element should be yielded.
+---@generic T
+---@param list T[] List to be iterated over.
+---@param predicate fun(value: T, index: number): T Callback to determine if element `value` should be yielded.
+---@return T[]
+function table.filter(list, predicate)
   local ret = {}
-  for key, value in pairs(tbl) do
-    local keep, k = callback(value, key)
-    ret[k or #ret + 1] = keep and value or nil
+  for i, value in ipairs(list) do
+    local keep = predicate(value, i)
+    if keep then table.insert(ret, value) end
   end
 
   return ret
 end
 
-function table.map(tbl, callback)
+---Replaces each element.
+---@generic T
+---@param list T[] List to be iterated over.
+---@param alter fun(value: T, index: number): T Callback to replace element `value`.
+---@return T[]
+function table.map(list, alter)
   local ret = {}
-  for key, value in pairs(tbl) do
-    local v, k = callback(value, key)
-    ret[k or #ret + 1] = v
+  for i, value in ipairs(list) do
+    local new = alter(value, i)
+    table.insert(ret, new)
   end
 
   return ret
 end
 
+---Checks if a file exists.
+---@param name string File path to be checked.
+---@return boolean
 function vim.fs.exists(name)
   local file, err = io.open(name, "r")
   if err ~= nil then return false end
