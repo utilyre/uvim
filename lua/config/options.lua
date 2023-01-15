ec44741d646f62a61dfca86bf456635d296c3a6a
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-15 23:38:49 +0330

    fix(options)!: disable relativenumber as we have leap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,7 +25,6 @@ vim.opt.shortmess = {
 vim.opt.colorcolumn = "+1"
 vim.opt.signcolumn = "yes:2"
 vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.showmode = false
 vim.opt.cursorline = true
 vim.opt.list = true
