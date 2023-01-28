9a9dc64713b388d3846c1813a40420ce8c0bed44
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-28 18:19:02 +0330

    fix(autocmds): give each autocmd its own augroup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,17 +1,15 @@
-local augroup = vim.api.nvim_create_augroup("config.autocmds", {})
-
 vim.api.nvim_create_autocmd("InsertLeave", {
-  group = augroup,
+  group = vim.api.nvim_create_augroup("config.autocmds#timeout", {}),
   callback = function() vim.opt.timeout = false end,
 })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
-  group = augroup,
+  group = vim.api.nvim_create_augroup("config.autocmds#timeout", {}),
   callback = function() vim.opt.timeout = true end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup,
+  group = vim.api.nvim_create_augroup("config.autocmds#yank", {}),
   callback = function()
     vim.highlight.on_yank({
       on_visual = false,
