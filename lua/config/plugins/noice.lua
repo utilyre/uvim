cbed3ba32b150076ad95e5b3aa7c8771fdb46197
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-01 17:16:39 +0330

    feat(noice): override cmp documentation menu

--- lua/config/plugins/noice.lua
+++ lua/config/plugins/noice.lua
@@ -38,6 +38,11 @@ function spec.config()
       },
     },
     lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
       progress = {
         format = {
           {
