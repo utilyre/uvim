4464dbd458983be8c0cf5b1215f78a647b6c63b1
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-09 21:58:05 +0330

    refactor(keymaps): re-order mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
-vim.keymap.set("i", "jk", "<esc>")
-vim.keymap.set("i", "kj", "<esc>")
 vim.keymap.set("n", "<s-y>", "y$")
-vim.keymap.set("n", "<leader>", "<nop>")
 
+vim.keymap.set("n", "<leader>", "<nop>")
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
 vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>")
 vim.keymap.set("n", "<leader>c", "<cmd>bdelete<cr>")
@@ -10,3 +8,6 @@ vim.keymap.set("n", "<leader>t", "<cmd>terminal<cr>")
 vim.keymap.set("n", "<leader>s", "<cmd>split<cr>")
 vim.keymap.set("n", "<leader>v", "<cmd>vsplit<cr>")
 vim.keymap.set("n", "<leader>u", "<cmd>nohlsearch<cr>")
+
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("i", "kj", "<esc>")
