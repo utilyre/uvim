1be539afb176a5033be08ceb7485c2f81a668384
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-09 22:21:54 +0330

    refactor(tokyonight): bring foreground before background

--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -11,7 +11,7 @@ function spec.config()
     on_colors = function(colors) colors.bg_statusline = colors.none end,
     on_highlights = function(highlights, colors)
       highlights["TelescopeSelectionCaret"] =
-        { background = colors.bg_visual, foreground = colors.teal }
+        { foreground = colors.teal, background = colors.bg_visual }
       highlights["TelescopeMultiIcon"] = { foreground = colors.green }
       highlights["TelescopeMultiSelection"] = { bold = true }
 
