44cc64f844c2311541812eaafe16aa413b7c4005
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-15 18:08:19 +0330

    fix(tokyonight): remove highlight overrides

--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -8,17 +8,6 @@ function spec.config()
 
   tokyonight.setup({
     style = "night",
-    on_highlights = function(highlights, colors)
-      highlights["TelescopeSelectionCaret"] =
-        { foreground = colors.teal, background = colors.bg_visual }
-      highlights["TelescopeMultiIcon"] = { foreground = colors.green }
-      highlights["TelescopeMultiSelection"] = { bold = true }
-
-      highlights["@text.note"] = { foreground = colors.blue2 }
-      highlights["@text.todo"] = { foreground = colors.green1 }
-      highlights["@text.warning"] = { foreground = colors.yellow }
-      highlights["@text.danger"] = { foreground = colors.red1 }
-    end,
   })
 
   tokyonight.load()
