8f4245ea6331c0e671c46526a61f4d8a486d4ce5
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-16 21:40:22 +0330

    feat(tokyonight): overwrite todo highlights

--- lua/config/plugins/tokyonight.lua
+++ lua/config/plugins/tokyonight.lua
@@ -12,6 +12,11 @@ function spec.config()
       highlights["TelescopePromptPrefix"] = { foreground = colors.blue }
       highlights["TelescopeMultiIcon"] = { foreground = colors.green }
       highlights["TelescopeMultiSelection"] = { bold = true }
+
+      highlights["@text.note"] = { bold = true, background = colors.blue2 }
+      highlights["@text.todo"] = { bold = true, background = colors.green1 }
+      highlights["@text.warning"] = { bold = true, background = colors.yellow }
+      highlights["@text.danger"] = { bold = true, background = colors.red1 }
     end,
   })
 
