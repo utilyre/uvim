f42e7b3f6be8409e95ccba1668388b27e6d4f534
Amirabbas Fazelinia <utilyre@proton.me>
2023-02-25 18:20:24 +0330

    perf(luasnip): remove sign autocmd

--- lua/config/plugins/luasnip.lua
+++ lua/config/plugins/luasnip.lua
@@ -11,33 +11,6 @@ function spec.config()
   })
 
   loader.lazy_load()
-
-  vim.fn.sign_define("LuasnipSignChoiceNode", {
-    text = vim.g.icons.widget.inline.Sort,
-    texthl = "LuasnipSignChoiceNode",
-  })
-
-  local group = vim.api.nvim_create_augroup("config.plugins.luasnip.sign", {})
-
-  vim.api.nvim_create_autocmd("User", {
-    group = group,
-    pattern = { "LuasnipChoiceNodeEnter", "LuasnipChangeChoice" },
-    callback = function(a)
-      vim.fn.sign_place(0, "LuasnipSign", "LuasnipSignChoiceNode", a.buf, {
-        lnum = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())[1],
-      })
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("User", {
-    group = group,
-    pattern = "LuasnipChoiceNodeLeave",
-    callback = function(a)
-      vim.fn.sign_unplace("LuasnipSign", {
-        buffer = a.buf,
-      })
-    end,
-  })
 end
 
 return spec
