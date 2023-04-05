8b020fe1e0d04d39a27fd2f59c374e8f896dedea
Amirabbas Fazelinia <utilyre@proton.me>
2023-04-05 23:58:03 +0330

    fix(lir): disable `'colorcolumn'` in its buffer

--- lua/config/plugins/lir.lua
+++ lua/config/plugins/lir.lua
@@ -33,6 +33,7 @@ function spec:config()
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "lir",
     callback = function()
+      vim.opt_local.colorcolumn = ""
       vim.opt_local.number = false
       vim.opt_local.relativenumber = false
       vim.opt_local.spell = false
