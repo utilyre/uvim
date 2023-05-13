621fce3f159a2b0d7f6c6a7a0930a85fb315ac9d
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-14 00:08:34 +0330

    refactor(dap): move sign_define calls above bindings

--- lua/config/plugins/dap.lua
+++ lua/config/plugins/dap.lua
@@ -13,14 +13,6 @@ function spec:config()
     vim.fs.normalize(vim.fn.stdpath("config") .. "/settings/adapters.lua")
   if vim.loop.fs_access(adapters_path, "R") then dofile(adapters_path) end
 
-  local binder = Binder.new({ "n" })
-  binder:bind("<leader>db", dap.toggle_breakpoint)
-  binder:bind("<leader>dc", dap.continue)
-  binder:bind("<leader>dn", dap.step_over)
-  binder:bind("<leader>di", dap.step_into)
-  binder:bind("<leader>do", dap.step_out)
-  binder:bind("<leader>dr", dap.repl.open)
-
   vim.fn.sign_define("DapBreakpoint", {
     text = icons.widget.breakpoint,
     texthl = "DiagnosticSignError",
@@ -29,6 +21,14 @@ function spec:config()
     text = icons.widget.stack,
     texthl = "DiagnosticSignInfo",
   })
+
+  local binder = Binder.new({ "n" })
+  binder:bind("<leader>db", dap.toggle_breakpoint)
+  binder:bind("<leader>dc", dap.continue)
+  binder:bind("<leader>dn", dap.step_over)
+  binder:bind("<leader>di", dap.step_into)
+  binder:bind("<leader>do", dap.step_out)
+  binder:bind("<leader>dr", dap.repl.open)
 end
 
 return spec
