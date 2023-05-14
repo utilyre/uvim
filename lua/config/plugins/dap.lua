e3f7e7513b406904e6cf5677b32983f7fc1418ec
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-14 19:54:45 +0330

    fix(dap): only define most keymaps when debugger is attached

--- lua/config/plugins/dap.lua
+++ lua/config/plugins/dap.lua
@@ -3,8 +3,8 @@ local Binder = require("config.binder")
 local spec = {
   "mfussenegger/nvim-dap",
   keys = {
-    "<leader>db",
     "<leader>dc",
+    "<leader>db",
   },
 }
 
@@ -19,20 +19,35 @@ function spec:config()
   vim.fn.sign_define("DapBreakpoint", { text = "", linehl = "DapBreakpoint" })
   vim.fn.sign_define("DapStopped", { text = "", linehl = "DapStopped" })
 
+  dap.listeners.after["event_initialized"].dap = function()
+    local binder = Binder.new({ "n" })
+
+    binder:clone():desc("Debug Next"):bind("<leader>dn", dap.step_over)
+    binder:clone():desc("Debug Into"):bind("<leader>di", dap.step_into)
+    binder:clone():desc("Debug Out"):bind("<leader>do", dap.step_out)
+    binder:clone():desc("Debug REPL"):bind("<leader>dr", dap.repl.open, {}, "")
+    binder
+      :clone()
+      :desc("Debug Scope")
+      :bind("<leader>ds", widgets.centered_float, widgets.scopes)
+  end
+
+  dap.listeners.before["event_terminated"].dap = function()
+    local binder = Binder.new({ "n" })
+
+    binder:unbind("<leader>dn")
+    binder:unbind("<leader>di")
+    binder:unbind("<leader>do")
+    binder:unbind("<leader>dr")
+    binder:unbind("<leader>ds")
+  end
+
   local binder = Binder.new({ "n" })
+  binder:clone():desc("Debug Continue"):bind("<leader>dc", dap.continue)
   binder
     :clone()
     :desc("Debug Breakpoint")
     :bind("<leader>db", dap.toggle_breakpoint)
-  binder:clone():desc("Debug Continue"):bind("<leader>dc", dap.continue)
-  binder:clone():desc("Debug Next"):bind("<leader>dn", dap.step_over)
-  binder:clone():desc("Debug Into"):bind("<leader>di", dap.step_into)
-  binder:clone():desc("Debug Out"):bind("<leader>do", dap.step_out)
-  binder:clone():desc("Debug REPL"):bind("<leader>dr", dap.repl.open, {}, "")
-  binder
-    :clone()
-    :desc("Debug Scope")
-    :bind("<leader>ds", widgets.centered_float, widgets.scopes)
 end
 
 return spec
