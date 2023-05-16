ef2c81e460351bef68050adc9d46f47f2868106b
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-17 00:54:43 +0330

    fix(opts): sensible tab options

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -34,9 +34,8 @@ vim.opt.pumheight = 20
 vim.opt.complete = {}
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.textwidth = 80
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.opt.spell = true
 vim.opt.spelllang:append({ "programming" })
