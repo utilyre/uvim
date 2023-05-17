4bbcc893b91866b7c25371ab3df6212512453041
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-17 23:55:20 +0330

    fix(opts): enable `cursorline`

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -20,6 +20,7 @@ vim.opt.colorcolumn = "+1"
 vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
 vim.opt.list = true
 vim.opt.listchars = { tab = icons.layout.tab .. "  " }
 vim.opt.fillchars:append({ eob = " " })
