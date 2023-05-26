e8bfc48f78d87ab3bddfd99c0b695e716e73e11b
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-26 16:44:18 +0330

    fix(cmp): return after fallback

--- lua/config/plugins/cmp.lua
+++ lua/config/plugins/cmp.lua
@@ -63,35 +63,67 @@ function spec:config()
     },
     mapping = {
       ["<c-space>"] = cmp.mapping(function(fallback)
-        if not cmp.visible() then fallback() end
+        if not cmp.visible() then
+          fallback()
+          return
+        end
+
         cmp.abort()
       end, { "i", "s" }),
       ["<c-y>"] = cmp.mapping(function(fallback)
-        if not cmp.visible() then fallback() end
+        if not cmp.visible() then
+          fallback()
+          return
+        end
+
         cmp.scroll_docs(-1)
       end, { "i", "s" }),
       ["<c-e>"] = cmp.mapping(function(fallback)
-        if not cmp.visible() then fallback() end
+        if not cmp.visible() then
+          fallback()
+          return
+        end
+
         cmp.scroll_docs(1)
       end, { "i", "s" }),
       ["<s-tab>"] = cmp.mapping(function(fallback)
-        if not cmp.visible() then fallback() end
+        if not cmp.visible() then
+          fallback()
+          return
+        end
+
         cmp.select_prev_item()
       end, { "i", "s" }),
       ["<tab>"] = cmp.mapping(function(fallback)
-        if not cmp.visible() then fallback() end
+        if not cmp.visible() then
+          fallback()
+          return
+        end
+
         cmp.select_next_item()
       end, { "i", "s" }),
       ["<cr>"] = cmp.mapping(function(fallback)
-        if cmp.get_selected_entry() == nil then fallback() end
+        if cmp.get_selected_entry() == nil then
+          fallback()
+          return
+        end
+
         cmp.confirm()
       end, { "i", "s" }),
       ["<c-p>"] = cmp.mapping(function(fallback)
-        if not luasnip.jumpable(-1) then fallback() end
+        if not luasnip.jumpable(-1) then
+          fallback()
+          return
+        end
+
         luasnip.jump(-1)
       end, { "i", "s" }),
       ["<c-n>"] = cmp.mapping(function(fallback)
-        if not luasnip.jumpable(1) then fallback() end
+        if not luasnip.jumpable(1) then
+          fallback()
+          return
+        end
+
         luasnip.jump(1)
       end, { "i", "s" }),
     },
