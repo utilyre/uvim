596ee0181bc6f278fcf3f7e605cad7f3fbd81a46
Amirabbas Fazelinia <utilyre@proton.me>
2023-05-27 23:12:43 +0330

    fix(barbecue): use the default autocmd

--- lua/config/plugins/barbecue.lua
+++ lua/config/plugins/barbecue.lua
@@ -14,26 +14,13 @@ local spec = {
 
 function spec:config()
   local barbecue = require("barbecue")
-  local ui = require("barbecue.ui")
 
   barbecue.setup({
-    create_autocmd = false,
     show_modified = true,
     kinds = icons.kind,
     symbols = {
       separator = icons.widget.nesting,
     },
-  })
-
-  vim.api.nvim_create_autocmd({
-    "BufReadPre",
-    "BufNewFile",
-    "CursorHold",
-    "InsertLeave",
-    "BufModifiedSet",
-  }, {
-    group = vim.api.nvim_create_augroup("config.plugins.barbecue.updater", {}),
-    callback = function() ui.update() end,
   })
 end
 
