43b4885f29f251b95f32af323d94a6afb8b969f0
Amirabbas Fazelinia <utilyre@proton.me>
2023-11-01 19:52:33 +0330

    fix(cmp): use vim.keymap instead of mapping option

--- lua/uvim/plugins/cmp.lua
+++ lua/uvim/plugins/cmp.lua
@@ -2,7 +2,6 @@ local icon = require("uvim.icon")
 
 local spec = {
     "hrsh7th/nvim-cmp",
-    main = "cmp",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "L3MON4D3/LuaSnip",
@@ -13,7 +12,6 @@ local spec = {
 }
 
 function spec:opts(opts)
-    local cmp = require("cmp")
     local luasnip = require("luasnip")
 
     return vim.tbl_deep_extend("force", opts, {
@@ -49,49 +47,17 @@ function spec:opts(opts)
             { name = "nvim_lsp" },
             { name = "buffer" },
         },
-        mapping = {
-            ["<c-p>"] = cmp.mapping(function(fallback)
-                if not cmp.visible() then
-                    fallback()
-                    return
-                end
-
-                cmp.select_prev_item()
-            end, { "i", "s" }),
-            ["<c-n>"] = cmp.mapping(function()
-                if not cmp.visible() then
-                    cmp.complete()
-                    return
-                end
-
-                cmp.select_next_item()
-            end, { "i", "s" }),
-            ["<c-y>"] = cmp.mapping(function(fallback)
-                if cmp.get_selected_entry() == nil then
-                    fallback()
-                    return
-                end
-
-                cmp.confirm()
-            end, { "i", "s" }),
-            ["<c-u>"] = cmp.mapping(function(fallback)
-                if not cmp.visible() then
-                    fallback()
-                    return
-                end
-
-                cmp.scroll_docs(-5)
-            end, { "i", "s" }),
-            ["<c-d>"] = cmp.mapping(function(fallback)
-                if not cmp.visible() then
-                    fallback()
-                    return
-                end
-
-                cmp.scroll_docs(5)
-            end, { "i", "s" }),
-        },
     })
+end
+
+function spec:config(opts)
+    local cmp = require("cmp")
+
+    cmp.setup(opts)
+
+    vim.keymap.set({ "i", "s" }, "<c-p>", cmp.select_prev_item)
+    vim.keymap.set({ "i", "s" }, "<c-n>", cmp.select_next_item)
+    vim.keymap.set({ "i", "s" }, "<c-y>", cmp.confirm)
 end
 
 return spec
